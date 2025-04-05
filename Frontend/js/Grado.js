f3369db3e2d21b5a5b6f84a06dacb44d14c29b01
gabrielhununez <gabrielhnunez@outlook.com.ar>
2025-04-05 12:08:36 -0300

    Actualizacion de area grados

--- Frontend/js/Grado.js
+++ Frontend/js/Grado.js
@@ -20,7 +20,8 @@ document.getElementById("grado-form").addEventListener("submit", function (e) {
     });
 });
 
-function cargarGrados() {
+function cargarGrados()
+{
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -29,21 +30,23 @@ function cargarGrados() {
 
             data.forEach(grado => {
                 let row = `<tr>
-                    <td>${grado._abreviatura}</td>
+                    <td class="centrar">${grado._abreviatura}</td>
                     <td>${grado._gradoCompleto}</td>
-                    <td>
-                        <button class="btn btn-info btn-sm cambio-btn">
-                            <span class="texto">Detalle</span>
-                            <img class="icono" src="../../img/detalle.svg" alt="detalle"/>
-                        </button>
-                        <button class="btn btn-warning btn-sm cambio-btn">
-                            <span class="texto">Editar</span>
-                            <img class="icono" src="../../img/editar.svg" alt="editar"/>
-                        </button>
-                        <button class="btn btn-danger btn-sm cambio-btn" onclick="Eliminar(${grado._idGrado})">
-                            <span class="texto">Eliminar</span>
-                            <img class="icono" src="../../img/eliminar.svg" alt="Eliminar"/>
-                        </button>
+                    <td class="centrar">
+                        <div class="btn-group">
+                            <button class="btn btn-info btn-sm cambio-btn">
+                                <span class="texto">Detalle</span>
+                                <img class="icono" src="../../img/detalle.svg" alt="detalle"/>
+                            </button>
+                            <button class="btn btn-warning btn-sm cambio-btn">
+                                <span class="texto">Editar</span>
+                                <img class="icono" src="../../img/editar.svg" alt="editar"/>
+                            </button>
+                            <button class="btn btn-danger btn-sm cambio-btn" href="../../Areas/Grado/eliminar.html">
+                                <span class="texto">Eliminar</span>
+                                <img class="icono" src="../../img/eliminar.svg" alt="Eliminar"/>
+                            </button>
+                        </div>
                     </td>
                 </tr>`;
                 tableBody.innerHTML += row;
